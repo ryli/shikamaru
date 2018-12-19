6f9738b42ca2f0a5853f732bd9270def01e789c2
yaf <lov.2014@gmail.com>
2018-12-19 18:32:09 +0800

    feat: mock

--- mock/users.js
+++ mock/users.js
@@ -1,4 +1,17 @@
-export default {
-  '/api/users': ['a', 'b']
+import mock from 'mockjs'
+import { delay } from 'roadhog-api-doc'
+
+const proxy = {
+  'GET /api/users': getUsers,
 }
+
+function getUsers(req, res) {
+  res.end({
+    'list|100': mock.mock({
+      'list|100': [{ name: '@city', 'value|1-100': 50, 'type|0-2': 1 }],
+    }),
+  })
+}
+
+export default delay(proxy, 1000)
 
