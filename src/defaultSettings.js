1a7f1dea664c3fc0886573ff87f1e46fec8fe9b9
yaf <lov.2014@gmail.com>
2018-12-21 16:33:25 +0800

    chore: setting

---
+++ src/defaultSettings.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // theme for nav menu
+  navTheme: 'dark',
+  // primary color of ant design
+  primaryColor: '#1890FF',
+  // nav menu position: sidemenu or topmenu
+  layout: 'sidemenu',
+  // layout of content: Fluid or Fixed, only works when layout is topmenu
+  contentWidth: 'Fluid',
+  // sticky header
+  fixedHeader: false,
+  // auto hide header
+  autoHideHeader: false,
+  // sticky siderbar
+  fixSiderbar: false,
+}
